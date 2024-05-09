575458a77b2e5e154fb0c9f14f60d8aa85f6abbb
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-05-09 01:36:49 +0000

    admin/user+post

---
+++ app/controllers/admin/post_comments_controller.rb
@@ -0,0 +1,5 @@
+class Admin::PostCommentsController < ApplicationController
+  before_action :authenticate_admin!
+  def index
+  end
+end
---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,30 @@
+class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to admin_user_path(user), notice: "ユーザー情報の更新に成功しました"
+    else
+      render 'edit'
+    end  
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :email, :is_active, :profile_image)
+  end
+end
