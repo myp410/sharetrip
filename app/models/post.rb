4164f37c9e442024dfbe09070a4c8c373ea0f459
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-05-10 01:45:09 +0000

    comp

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
   has_many :itineraries, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags #投稿画面で表示したいから中間テーブル経由でのこの記載をする
 
   validates :title, presence: true
   validates :start_date, presence: true
@@ -20,5 +22,38 @@ class Post < ApplicationRecord
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
+  
+  def save_tags(tags)
+    tags.each do |new_tags|
+      self.tags.find_or_create_by(name: new_tags)
+    end  
+  end
+  
+  def update_tags(latest_tags)
+    if self.tags.empty? #既存のタグがない場合、追加のみ行う 初回ではタグ登録をしていなく、２回目でタグ登録をした
+      latest_tags.each do |latest_tag|
+        self.tags.find_or_create_by(name: latest_tag)
+      end
+    elsif latest_tags.empty? #更新対象のタグがなかったら既存のタグを全て削除　元々タグを登録していて、からで更新した時
+      self.tags.each do |tag|
+        self.tags.delete(tag)
+      end
+    else #既存のタグも更新対象のタグもある場合、差分更新
+      current_tags = self.tags.pluck(:name)
+      old_tags = current_tags - latest_tags #左側を基準に考える b-abc = ""
+      new_tags = latest_tags - current_tags #abc-b=ac
+      
+      old_tags.each do |old_tag|
+        tag = self.tags.find_by(name: old_tag)
+        self.tags.delete(tag) if tag.present?
+      end  
+      
+      new_tags.each do |new_tag|
+        self.tags.find_or_create_by(name: new_tag)
+      end  
+    end  
+  end
+  
+  enum status: { published: 0, draft: 1,  unpublished: 2 }
 
 end
