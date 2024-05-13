75ea78457836787fa347663cc926ce97a07c1784
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-05-13 03:49:02 +0000

    comp

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -5,24 +5,19 @@ class Public::PostCommentsController < ApplicationController
     @new_comment = PostComment.new
     @post_tags = @post.tags
   end
-  
+
   def create
     @post = Post.find(params[:post_id])
     @new_comment = current_user.post_comments.new(post_comment_params)
     @new_comment.post_id = @post.id
-    if @new_comment.save
-      flash[:notice] = "コメントの投稿に成功しました"
-    else
-      flash.now[:alert] = "コメントの投稿に失敗しました"
-      render 'index'
-    end  
+    @new_comment.save
   end
 
   def edit
     @post = Post.find(params[:post_id])
     @post_comment = PostComment.find(params[:id])
   end
-  
+
   def update
     post_comment = PostComment.find(params[:id])
     if post_comment.update(post_comment_params)
@@ -30,18 +25,17 @@ class Public::PostCommentsController < ApplicationController
     else
       flash.now[:alert] = "コメントの更新に失敗しました"
       render 'edit'
-    end  
+    end
   end
-  
+
   def destroy
     @post_comment = PostComment.find(params[:id])
     @post_comment.destroy
-    flash[:notice] = "コメントを削除しました"
   end
-  
+
   private
-  
+
   def post_comment_params
     params.require(:post_comment).permit(:comment, :post_id, :user_id)
-  end  
+  end
 end
