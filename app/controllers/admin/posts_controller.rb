057a47efc8ca9aedbf4b16b56023daf7bfa71726
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-05-13 04:40:46 +0000

    [fix]posts/bug

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,7 @@
 class Admin::PostsController < ApplicationController
   before_action :authenticate_admin!
   def index
-    @posts = Post.order(create_at: :desc)
+    @posts = Post.order(created_at: :desc)
   end
   
   def show
