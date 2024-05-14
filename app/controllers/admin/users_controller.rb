83aa7a26acecb1675bcdd95950afb2b33d963aad
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-05-14 03:38:53 +0000

    comp

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,11 +10,7 @@ class Admin::UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @owner = @user.groups.find_by(params[:owner_id])
     if @user.update(user_params)
-      if @user.is_active == "false"
-        @owner.groups.update_all(is_active: "false")
-      end
       redirect_to admin_user_path(@user), notice: "ユーザー情報の更新に成功しました"
     else
       render 'edit'
