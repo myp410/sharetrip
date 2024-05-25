0d768bc10c332abafa9c68152c1c6db0f7c1d718
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-05-25 01:55:09 +0000

    footer/relationshipbug

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -32,7 +32,7 @@ class Public::SessionsController < Devise::SessionsController
   end
   
   def after_sign_in_path_for(resource)
-    users_my_page_path(@user)
+    users_my_page_path(current_user)
   end
   
   def after_sign_out_path_for(resource)
