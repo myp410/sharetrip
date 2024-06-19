118fc1059fe723d2bfa1571770ec1fe55c303483
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-06-19 01:12:23 +0000

    comp

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,17 +22,17 @@
 # Rails.rootを使用するために必要
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 # cronを実行する環境変数
-rails_env = ENV['RAILS_ENV'] || :development
+rails_env = Rails.env.to_sym
 # cronを実行する環境変数をセット
 set :environment, rails_env
 # cronのログの吐き出し場所
-set :output, "#{Rails.root}/log/cron.log"
+set :output, 'log/cron.log'
 
 #30分に一度、data_resetメゾッドが実行される記述
 #テストする際は下記を3.minuteとかに変えた方がいいかもしれません。
-every 3.minute do
+every 30.minute do
   begin
-    runner "Guestuser::DataGuest.data_reset"
+    runner "Batch::DataReset.data_reset"
   rescue => e
     Rails.logger.error("aborted rails runner")
     raise e
--- lib/data_guest.rb
+++
@@ -1,9 +0,0 @@
-class Guestuser::DataGuest
-
-  # guestuserの投稿を削除
-  def self.data_reset
-    user = User.find_by(email: "guest@example.com")
-    user.posts.destroy_all
-    user.post_comments.destroy_all
-  end
-end
