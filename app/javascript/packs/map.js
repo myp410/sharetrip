109b9dbae36f0a13d1fd0bdc02a3e9e86930e644
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-06-23 11:42:50 +0000

    bug/lang

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,21 +1,25 @@
-// ブートストラップ ローダ
-(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-  key: process.env.Maps_API_Key
-});
+function initMap() {
 
+        var test = {lat: <%= @itinerary.latitude %>, lng: <%= @itinerary.longitude %>};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 15,
+            center: test
+        });
+        var transitLayer = new google.maps.TransitLayer();
+        transitLayer.setMap(map);
 
-// ライブラリの読み込み
-let map;
+        var contentString = '住所：<%= @itinerary.place %>';
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
 
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
+        var marker = new google.maps.Marker({
+            position:test,
+            map: map,
+            title: contentString
+        });
 
-  // 地図の中心と倍率は公式から変更しています。
-  map = new Map(document.getElementById("map"), {
-    center: { lat: 35.681236, lng: 139.767125 }, 
-    zoom: 15,
-    mapTypeControl: false
-  });
-}
-
-initMap()
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
+        });
+    }
