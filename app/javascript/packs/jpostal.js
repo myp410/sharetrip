4280f99ee74f11afa1c9b853c0fd746f53e8e87d
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-06-23 09:54:20 +0000

    menter

--- app/javascript/packs/jpostal.js
+++ app/javascript/packs/jpostal.js
@@ -1,9 +1,9 @@
 function jpostal() {
   $('#zipcode').jpostal({
     postcode : ['#zipcode'],
-    address : {
-      '#post_image_address': '%3%4%5'
+    place : {
+      '#itinerary_place': '%3%4%5'
     }
-  });
+  })
 }
 $(document).on("turbolinks:load", jpostal);
