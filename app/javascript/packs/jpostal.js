53591d8941b3e3ed0c3ca0c3bb86bf5414693e98
EC2 Default User <ec2-user@ip-172-31-26-150.ap-northeast-1.compute.internal>
2024-07-03 23:59:03 +0000

    fix/file

--- app/javascript/packs/jpostal.js
+++
@@ -1,9 +0,0 @@
-function jpostal() {
-  $('#zipcode').jpostal({
-    postcode : ['#zipcode'],
-    address : {
-      '#itinerary_place': '%3%4%5'
-    }
-  })
-}
-$(document).on("turbolinks:load", jpostal);
